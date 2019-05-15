5d4951b31097a0c1819774713d4e990b77c7c963
cl2227619761 <cl2227619761@gmail.com>
2019-05-15 09:13:08 +0800

    update

--- data/dataset.py
+++ data/dataset.py
@@ -7,3 +7,39 @@ from __future__ import print_function
 
 import torch
 from torchvision import transforms
+from skimage import transform
+
+
+def img_normalize(img):
+    """对图片进行标准化处理"""
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+    img = torch.from_numpy(img)
+    return normalize(img)
+
+
+def inverse_normalize(img):
+    """将标准化后的图片转回来"""
+    img = img * 0.225 + 0.45
+    return img.clip(min=0, max=1) * 225
+
+
+def preprocess(img, min_size=600, max_size=1000):
+    """对图像进行预处理"""
+    channel, height, width = img.shape
+    scale1 = min_size / min(height, width)
+    scale2 = max_size / max(height, width)
+    scale = min(scale1, scale2)
+    img = img / 255.
+    img = transform.resize(
+        img, (channel, height * scale, width * scale), mode="reflect",
+        anti_aliasing=False
+    )
+    return img_normalize(img)
+
+
+def resize_bbox(bbox, in_size, out_size):
+    """根据图像的变化对真实框做相应的变化"""
+
--- data/util.py
+++ data/util.py
@@ -4,6 +4,8 @@
 """
 import numpy as np
 from PIL import Image
+from torchvision import transforms
+from skimage import transform
 
 
 def read_image(path, dtype=np.float32, color=True):
@@ -22,3 +24,46 @@ def read_image(path, dtype=np.float32, color=True):
         return img[np.newaxis]
     else:
         return img.transpose((2, 0, 1))
+
+
+def img_normalize(img):
+    """图像标准化处理"""
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+    img = torch.from_numpy(img)
+    return normalize(img)
+
+
+def inverse_normalize(img):
+    """标准化处理的反过程"""
+    img = img * 0.225 + 0.45
+    return img.clip(min=0, max=1) * 255
+
+
+def preprocess(img, min_size=600, max_size=1000):
+    """图片预处理"""
+    C, H, W = img.shape
+    scale1 = min_size / min(H, W)
+    scale2 = max_size / max(H, W)
+    scale = min(scale1, scale2)
+    img = img / 255.
+    img = transform.resize(
+        img, (C, H * scale, W * scale),
+        mode="reflect", anti_aliasing=False
+    )
+    return img_normalize(img)
+
+
+def resize_bbox(bbox, in_size, out_size):
+    """bbox随着图片的预处理过程做相应变化"""
+    bbox = bbox.copy()
+    y_scale = float(out_size[0] / in_size[0])
+    x_scale = float(out_size[1] / in_size[1])
+    bbox[:, 0::2] = bbox[:, 0::2] * y_scale
+    bbox[:, 1::2] = bbox[:, 1::2] * x_scale
+    return bbox
+
+
+
