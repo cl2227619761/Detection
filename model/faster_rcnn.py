b0f739cc3e37eb336ee7bee070d3a162bff8439d
cl2227619761 <cl2227619761@gmail.com>
2019-05-15 17:16:37 +0800

    update model

--- model/faster_rcnn.py
+++ model/faster_rcnn.py
@@ -6,3 +6,40 @@ from __future__ import division
 from __future__ import print_function
 
 import torch
+import torch.nn as nn
+
+
+def nograd(func):
+    """修饰器，非训练阶段使用"""
+    def wrapper(*args, **kwargs):
+        with torch.no_grad():
+            return func(*args, **kwargs)
+    return wrapper
+
+
+class FasterRCNN(nn.Module):
+    """Faster RCNN基类
+    参数：
+        extractor: 特征提取网络
+        rpn: 候选框生成网络
+        head: 分类和回归头
+        loc_normalize_mean: 位置估计的均值
+        loc_normalize_std: 位置估计的标准差
+    返回：
+        forward过程返回预测框的偏移量，得分，坐标，预测框的索引
+    """
+
+    def __init__(
+            self, extractor, rpn, head,
+            loc_normalize_mean, loc_normalize_std
+    ):
+        super(FasterRCNN, self).__init__()
+        self.extractor = extractor
+        self.rpn = rpn
+        self.head = head
+
+        self.loc_normalize_mean = loc_normalize_mean
+        self.loc_normalize_std = loc_normalize_std
+
+    def forward(self, x):
+        features = self.extractor(x)
---
+++ model/rpn.py
@@ -0,0 +1,3 @@
+"""
+region proposal network
+"""
