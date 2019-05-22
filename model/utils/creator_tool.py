d6c23a1f1d5ff051b482ce9f351d6262899407f2
cl2227619761 <cl2227619761@gmail.com>
2019-05-22 22:44:08 +0800

    更新了anchortargetcreator和proposaltargetcreator

--- model/utils/creator_tool.py
+++ model/utils/creator_tool.py
@@ -4,7 +4,7 @@
 import numpy as np
 import cupy as cp
 
-from utils.bbox_tools import loc2bbox, bbox_iou
+from utils.bbox_tools import loc2bbox, bbox2loc, bbox_iou
 from utils.nms import non_maximum_suppression
 
 
@@ -103,6 +103,7 @@ class AnchorTargetCreator:
         返回：赋予的偏移量和标签
         """
         img_h, img_w = img_size
+        n_anchor = len(anchor)
         # 得到不超出图像边界的锚点框的索引
         inside_index = _get_inside_index(anchor, img_h, img_w)
         # 得到不超出图像边界的锚点框
@@ -110,6 +111,12 @@ class AnchorTargetCreator:
         # 然后就可以创建标签了
         argmax_iou, label = self._create_label(inside_index, anchor, bbox)
 
+        # 计算锚点框相对于其对应的最大iou的真实框的偏移量作为真实偏移量
+        loc = bbox2loc(src_bbox=anchor, dst_bbox=bbox[argmax_iou])
+        # 将偏移量和标签整理成(count, 4)和(count,)的形式
+        label = _umap(data=label, count=n_anchor, index=inside_index, fill=-1)
+        loc = _umap(data=loc, count=n_anchor, index=inside_index, fill=0)
+        return loc, label
 
     def _create_label(self, inside_index, anchor, bbox):
         """创建标签的函数"""
@@ -165,8 +172,6 @@ class AnchorTargetCreator:
         return argmax_iou, max_ious, gt_argmax_iou
 
 
-
-
 def _get_inside_index(anchor, img_h, img_w):
     """得到不超出边界的锚点框的索引"""
     inside_index = np.where(
@@ -176,3 +181,84 @@ def _get_inside_index(anchor, img_h, img_w):
         (anchor[:, 3] <= img_w)
     )[0]
     return inside_index
+
+
+def _umap(data, count, index, fill=0):
+    """将标签整理成(count,)的形状，将偏移量整理成(count, 4)的形状"""
+    # 对于标签会有：
+    if len(data.shape) == 1:
+        ret = np.empty((count,), dtype=data.dtype)
+        ret.fill(fill)
+        ret[index] = data
+    # 对于偏移量会有：
+    else:
+        ret = np.empty((count,) + data.shape[1:], dtype=data.dtype)
+        ret.fill(fill)
+        ret[index, :] = data
+    return ret
+
+
+class ProposalTargetCreator:
+    """为候选框产生真实的偏移量和标签，用于训练fast rcnn部分"""
+
+    def __init__(
+            self, n_sample=128,
+            pos_ratio=0.25, pos_iou_thresh=0.5,
+            neg_iou_thresh_hi=0.5, neg_iou_thresh_lo=0.0
+    ):
+        self.n_sample = n_sample
+        self.pos_ratio = pos_ratio
+        self.pos_iou_thresh = pos_iou_thresh
+        self.neg_iou_thresh_hi = neg_iou_thresh_hi
+        self.neg_iou_thresh_lo = neg_iou_thresh_lo
+
+    def __call__(
+            self, roi, bbox, label,
+            loc_normalize_mean=(0., 0., 0., 0.),
+            loc_normalize_std=(0.1, 0.1, 0.2, 0.2)
+    ):
+        roi = np.concatenate(roi, bbox, axis=0)
+        # 图片上要抽取的阳性框个数
+        pos_roi_per_img = np.round(self.n_sample * self.pos_ratio)
+        # 计算和真实框的iou
+        iou = bbox_iou(roi, bbox)
+        # 和roi的iou较大的那个真实框的索引
+        gt_assignment = iou.argmax(axis=1)
+        # 那个较大的iou是多少
+        max_iou = iou.argmax(axis=1)
+        # 去除背景类?
+        gt_roi_label = label[gt_assignment] + 1
+
+        pos_index = np.where(max_iou >= self.pos_iou_thresh)[0]
+        # 从非背景类中选取一定数量的roi
+        pos_roi_per_this_img = int(min(pos_roi_per_img, pos_index.size))
+        if pos_index.size > 0:
+            pos_index = np.random.choice(
+                pos_index, size=pos_roi_per_this_img, replace=False
+            )
+        # 从背景类中选取一定数量的背景
+        neg_index = np.where(
+            (max_iou >= self.neg_iou_thresh_lo) &
+            (max_iou < self.neg_iou_thresh_hi)
+        )[0]
+        neg_roi_per_img = self.n_sample - pos_roi_per_this_img
+        neg_roi_per_this_img = int(min(neg_roi_per_img, neg_index.size))
+        if neg_index.size > 0:
+            neg_index = np.random.choice(
+                neg_index, size=neg_roi_per_this_img, replace=False
+            )
+        # 选取
+        keep_index = np.append(pos_index, neg_index)
+        gt_roi_label = gt_roi_label[keep_index]
+        gt_roi_label[pos_roi_per_this_img:] = 0  # 背景类的标签设为0
+        sample_roi = roi[keep_index]
+
+        # 为这些候选框赋予真实的偏移量和标签
+        gt_roi_loc = bbox2loc(
+            src_bbox=sample_roi, dst_bbox=bbox[gt_assignment[keep_index]]
+        )
+        gt_roi_loc = (
+            (gt_roi_loc - np.array(loc_normalize_mean, np.float32)) /
+            np.array(loc_normalize_std, np.float32)
+        )
+        return sample_roi, gt_roi_loc, gt_roi_label
