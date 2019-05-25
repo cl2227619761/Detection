bc356282af6d6529d6c01052ae393788502ac09c
cl2227619761 <cl2227619761@gmail.com>
2019-05-25 18:38:57 +0800

    更新适应ALL数据集

--- data/bbox_dataset.py
+++ data/bbox_dataset.py
@@ -6,7 +6,7 @@ import xml.etree.ElementTree as ET
 
 import numpy as np
 
-from util import read_image
+from data.util import read_image
 
 
 class BboxDataset:
@@ -39,7 +39,10 @@ class BboxDataset:
 
     def __getitem__(self, index):
         id_ = self.ids[index]
-        anno_path = os.path.join(self.data_dir, "Annotations", id_ + ".xml")
+        # import ipdb; ipdb.set_trace()  本步是为了避免检查是否出现.jpg.xml
+        anno_path = os.path.join(
+            self.data_dir, "Annotations", id_.split(".")[0] + ".xml"
+        )
         anno = ET.parse(anno_path)
         bbox = list()
         label = list()
@@ -51,42 +54,65 @@ class BboxDataset:
             bndbox = obj.find("bndbox")
             bbox.append([
                 int(bndbox.find(tag).text) - 1
-                for tag in ("ymin", "xmin", "ymax", "ymin")
+                for tag in ("ymin", "xmin", "ymax", "xmax")
             ])
-            name = obj.find("name").text.lower().strip()
+            # name = obj.find("name").text.lower().strip()
+            # 这步是针对中文标签的
+            name = obj.findtext("name").strip()
             label.append(BBOX_LABEL_NAMES.index(name))
         bbox = np.stack(bbox).astype(np.float32)
+        # ---------------加入去除ALL图片中超出边界及噪声框------------------------#
+        # import ipdb; ipdb.set_trace()
+        bbox[:, [0, 2]] = np.clip(bbox[:, [0, 2]], a_min=0, a_max=1200)
+        bbox[:, [1, 3]] = np.clip(bbox[:, [1, 3]], a_min=0, a_max=1920)
+        keep_bbox = []
+        bbox_hs = bbox[:, 2] - bbox[:, 0]
+        bbox_ws = bbox[:, 3] - bbox[:, 1]
+        for i in range(len(bbox_hs)):
+            if bbox_hs[i] > 50 and bbox_ws[i] > 50:
+                keep_bbox.append(i)
+        bbox = bbox[keep_bbox]
         label = np.stack(label).astype(np.int32)
+        label = label[keep_bbox]
         difficult = np.stack(difficult).astype(np.bool)
+        difficult = difficult[keep_bbox]
 
-        img_path = os.path.join(self.data_dir, "JPEGImages", id_ + ".jpg")
+        # img_path = os.path.join(self.data_dir, "JPEGImages", id_ + ".jpg")
+        img_path = os.path.join(
+            self.data_dir, "JPEGImages", id_.split(".")[0] + ".jpg"
+        )
         img = read_image(img_path)
         return img, bbox, label, difficult
 
 
 # 所有的类别名称，总共有20类
 BBOX_LABEL_NAMES = (
-    'aeroplane',
-    'bicycle',
-    'bird',
-    'boat',
-    'bottle',
-    'bus',
-    'car',
-    'cat',
-    'chair',
-    'cow',
-    'diningtable',
-    'dog',
-    'horse',
-    'motorbike',
-    'person',
-    'pottedplant',
-    'sheep',
-    'sofa',
-    'train',
-    'tvmonitor'
+    '正常',
+    '异常',
 )
+
+# BBOX_LABEL_NAMES = (
+#     'aeroplane',
+#     'bicycle',
+#     'bird',
+#     'boat',
+#     'bottle',
+#     'bus',
+#     'car',
+#     'cat',
+#     'chair',
+#     'cow',
+#     'diningtable',
+#     'dog',
+#     'horse',
+#     'motorbike',
+#     'person',
+#     'pottedplant',
+#     'sheep',
+#     'sofa',
+#     'train',
+#     'tvmonitor'
+# )
 
 
 def main():
