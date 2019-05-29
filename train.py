72666967f37f04e3205485740585deb3f867ddb3
cl2227619761 <cl2227619761@gmail.com>
2019-05-29 17:03:57 +0800

    完成了训练，验证，测试以及loss, mAP导入csv文件，最好的模型的保存

--- train.py
+++ train.py
@@ -3,12 +3,15 @@
 """
 import sys
 sys.path.append("./")
+import csv
 
 import torch
 from torch.utils.data import DataLoader
 import progressbar as pb
+import numpy as np
 
 from lib.config import OPT
+from lib.csv_tool import loss_writer, map_writer
 from data.dataset import Dataset, TestDataset
 from model.faster_rcnn_vgg16 import FasterRCNNVGG16
 from model.utils.array_tool import scalar
@@ -56,7 +59,22 @@ def train(**kwargs):
         num_workers=OPT.num_workers
     )
     # 测试集
-    testset = TestDataset(opt=OPT)
+    valtestset = TestDataset(opt=OPT)
+    np.random.seed(1234)
+    split_seq = np.random.choice(
+        range(len(valtestset)), size=int(0.5 * len(valtestset)), replace=False
+    )
+    valset = torch.utils.data.Subset(valtestset, indices=split_seq)
+    testset = torch.utils.data.Subset(
+        valtestset, indices=[
+            i for i in range(len(valtestset))
+            if i not in split_seq
+        ]
+    )
+    val_dataloader = DataLoader(
+        dataset=valset, batch_size=1, shuffle=False,
+        num_workers=OPT.num_workers, pin_memory=True
+    )
     test_dataloader = DataLoader(
         dataset=testset, batch_size=1, shuffle=False,
         num_workers=OPT.num_workers, pin_memory=True
@@ -65,6 +83,9 @@ def train(**kwargs):
     faster_rcnn = FasterRCNNVGG16()
     print("模型加载完成")
     trainer = FasterRCNNTrainer(faster_rcnn).cuda()
+    train_loss = []
+    val_loss = []
+    val_mAP = []
 
     best_map = 0  # 最好的map
     lr_ = OPT.lr  # 学习率
@@ -80,23 +101,51 @@ def train(**kwargs):
             trainer.train_step(
                 imgs=img, bboxes=bbox, labels=label, scale=scale
             )
+        train_loss.append(trainer.get_meter_data())
         print("train:", trainer.get_meter_data())
             # if (ii + 1) % OPT.plot_every == 0:
             #     print(trainer.get_meter_data())
         trainer.eval()
         for jj, (img, size, _, bbox, label, _) in pb.progressbar(
-            enumerate(test_dataloader), max_value=len(test_dataloader)
+            enumerate(val_dataloader), max_value=len(val_dataloader)
         ):
             img, bbox, label = img.cuda(), bbox.cuda(), label.cuda()
             trainer.val_step(img, size, bbox, label)
+        val_loss.append(trainer.get_meter_data())
         print("val:", trainer.get_meter_data())
         eval_result = evaluate(
-            dataloader=test_dataloader, faster_rcnn=faster_rcnn,
-            test_num=OPT.test_num
+            dataloader=val_dataloader, faster_rcnn=faster_rcnn,
+            test_num=len(val_dataloader)
         )
+        val_mAP.append(eval_result["mAP"])
         print("mAP: %.4f" % eval_result["mAP"])
         print()
         trainer.train()
+
+        # 将损失函数，map写入csv文件
+        loss_writer(train_loss, out_path=OPT.train_loss_csv_path)
+        loss_writer(val_loss, OPT.val_loss_csv_path)
+        map_writer(val_mAP, out_path=OPT.map_csv_path)
+
+        if eval_result["mAP"] > best_map:
+            best_map = eval_result["mAP"]
+            best_path = trainer.save(
+                save_path=OPT.model_save_path, best_map=best_map
+            )
+        if epoch == 9:
+            trainer.load(best_path)
+            trainer.faster_rcnn.scale_lr(OPT.lr_decay)
+    print("===========训练结束============")
+    print("===========开始测试=============")
+    trainer.load(best_path)
+    trainer.eval()
+    test_result = evaluate(
+        dataloader=test_dataloader, faster_rcnn=trainer.faster_rcnn,
+        test_num=len(test_dataloader)
+    )
+    print("==========测试结束===========")
+    print("best mAP: %.4f" % best_map)
+    print("test mAP: %.4f" % test_result["mAP"])
 
 
 # def main():
