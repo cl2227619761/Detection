f5c396983dffbabc07c58a8e122c0b78144b074e
cl2227619761 <cl2227619761@gmail.com>
2019-06-06 17:08:39 +0800

    update

--- model/utils/backbone_util.py
+++ model/utils/backbone_util.py
@@ -3,7 +3,8 @@
 """
 from collections import OrderedDict
 
-from torchvision.models import resnet, resnet18
+from torchvision.models import resnet18
+import resnet
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -43,6 +44,7 @@ class IntermediaLayerGetter(nn.ModuleDict):
             return ValueError("要提取的层在模型中不存在！")
 
         orig_return_layers = return_layers  # 要提取的层的字典
+        return_layers = {k: v for k, v in return_layers.items()}
         layers = OrderedDict()  # 用来存储要提取的层的OrderedDict字典
         for name, module in model.named_children():
             layers[name] = module
@@ -57,7 +59,6 @@ class IntermediaLayerGetter(nn.ModuleDict):
 
     def forward(self, x):
         out = OrderedDict()  # 输出的结果是OrderedDict
-        import ipdb; ipdb.set_trace()
         for name, module in self.named_children():  # self为提取出的层模型
             x = module(x)
             if name in self.return_layers:
@@ -70,6 +71,76 @@ class FeaturePyramidNetwork(nn.Module):
     """特征金字塔网络"""
 
     def __init__(self, in_channels_list, out_channels, extra_blocks=None):
+        super(FeaturePyramidNetwork, self).__init__()
+        self.inner_blocks = nn.ModuleList()  # 用来放置1x1卷积层
+        self.layer_blocks = nn.ModuleList()  # 用来放置3x3卷积层
+        for in_channels in in_channels_list:
+            inner_block_module = nn.Conv2d(in_channels, out_channels, 1)
+            layer_block_module = nn.Conv2d(
+                out_channels, out_channels, 3, padding=1
+            )
+            self.inner_blocks.append(inner_block_module)
+            self.layer_blocks.append(layer_block_module)
+
+        for module in self.children():
+            if isinstance(module, nn.Conv2d):
+                nn.init.kaiming_uniform_(module.weight, a=1)
+                nn.init.constant_(module.bias, 0)
+
+        if extra_blocks is not None:
+            assert isinstance(extra_blocks, ExtraFPNBlock)
+        self.extra_blocks = extra_blocks
+
+    def forward(self, x):
+        names = list(x.keys())
+        x = list(x.values())
+
+        # 最顶层特征图进行1x1卷积改变通道数
+        last_inner = self.inner_blocks[-1](x[-1])
+        results = []  # 用来放置每一层金字塔处理后得到的特征
+        results.append(self.layer_blocks[-1](last_inner))
+
+        # 遍历除了顶层特征图以外的所有特征图，进行特征图的整合
+        for feature, inner_block, layer_block in zip(
+                x[:-1][::-1], self.inner_blocks[:-1][::-1],
+                self.layer_blocks[:-1][::-1]
+        ):
+            if not inner_block:
+                continue
+            # 进行横向的1x1卷积操作
+            inner_lateral = inner_block(feature)
+            # 得到特征图尺寸，用于上采样插值
+            feat_shape = inner_lateral.shape[-2:]
+            # 上采样过程
+            inner_top_down = F.interpolate(
+                input=last_inner, size=feat_shape, mode="nearest"
+            )
+            last_inner = inner_lateral + inner_top_down
+            results.insert(0, layer_block(last_inner))
+
+        if self.extra_blocks is not None:
+            results, names = self.extra_blocks(results, x, names)
+
+        out = OrderedDict([(k, v) for k, v in zip(names, results)])
+
+        return out  # 返回的是金字塔的每一层的经过特征整合之后的特征
+
+
+class ExtraFPNBlock(nn.Module):
+    """在金字塔之后接的额外的层"""
+
+    def forward(self, results, x, names):
+        pass
+
+
+class LastLevelMaxPool(ExtraFPNBlock):
+    """继承ExtraFPNBlock，接的是最大池化"""
+
+    def forward(self, results, x, names):
+        names.append("pool")
+        results.append(F.max_pool2d(results[-1], 1, 2, 0))
+        return results, names
+
 
 class BackboneWithFPN(nn.Sequential):
     """添加FPN到特征提取器"""
@@ -78,21 +149,31 @@ class BackboneWithFPN(nn.Sequential):
             self, backbone, return_layers, in_channels_list, out_channels
     ):
         body = IntermediaLayerGetter(backbone, return_layers)
+        fpn = FeaturePyramidNetwork(
+            in_channels_list=in_channels_list,
+            out_channels=out_channels,
+            extra_blocks=LastLevelMaxPool()
+        )
+        # body和fpn都是OrderedDict，对其进行合并，得到加了FPN的特征提取器
+        super(BackboneWithFPN, self).__init__(OrderedDict([
+            ("body", body), ("fpn", fpn)
+        ]))
+        self.out_channels = out_channels
 
 
 def resnet_fpn_backbone(backbone_name):
     """为resnet添加fpn"""
-    backebone = resnet.__dict__[backbone_name](
+    backbone = resnet.__dict__[backbone_name](
         pretrained=True, norm_layer=FrozenBatchNorm2d
     )
-    for name, param in backebone.named_paramters():
+    for name, param in backbone.named_parameters():
         if "layer2" not in name and "layer3" not in name and "layer4" not in  \
                 name:
             param.requires_grad_(False)
 
     return_layers = {"layer1": 0, "layer2": 1, "layer3": 2, "layer4": 3}
 
-    in_channels_stage2 = 256  # 这是rpn中间层的卷积层输入通道数
+    in_channels_stage2 = 64
     # 这是fpn特征们的通道数
     in_channels_list = [
         in_channels_stage2,
@@ -101,16 +182,25 @@ def resnet_fpn_backbone(backbone_name):
         in_channels_stage2 * 8
     ]
     out_channels = 256  # 经过rpn卷积层的输出通道数为256
+    return BackboneWithFPN(
+        backbone=backbone, return_layers=return_layers,
+        in_channels_list=in_channels_list, out_channels=out_channels
+    )
 
 
 def main():
     """调试用函数"""
     # backbone = resnet_fpn_backbone("resnet18")
-    model = resnet18(pretrained=True)
-    img = torch.rand(1, 3, 224, 224)
-    return_layers = {"layer1": "feat1", "layer2": "feat2"}
-    new_model = IntermediaLayerGetter(model, return_layers)
-    out = new_model(img)
+    # model = resnet.__dict__["resnet18"](pretrained=True)
+    # img = torch.rand(1, 3, 224, 224)
+    # return_layers = {"layer1": "feat1", "layer2": "feat2"}
+    # new_model = IntermediaLayerGetter(model, return_layers)
+    # import ipdb; ipdb.set_trace()
+    # out = new_model(img)
+    backbone = resnet_fpn_backbone("resnet18")
+    img = torch.rand(1, 3, 223, 224)
+    import ipdb; ipdb.set_trace()
+    feat = backbone(img)
 
 
 if __name__ == "__main__":
